e1859015c7cdcc77d9ace9c478a28862c6b01f2d
Abhishek Garg <abhigarg28@gmail.com>
2022-05-08 14:24:28 -0700

    Able to create a github repository.

--- live/github/main.tf
+++ live/github/main.tf
@@ -1,4 +1,7 @@
 module "create_an_example_repo" {
+  providers = {
+    github = github
+  }
   source           = "../../modules/github"
   repo_name        = "example_repo"
   repo_description = "example description for the repo"
--- live/github/providers.tf
+++ live/github/providers.tf
@@ -1,4 +1,3 @@
 provider "github" {
-  owner = abhigarg28
   token = var.abhigarg_github_token
 }
