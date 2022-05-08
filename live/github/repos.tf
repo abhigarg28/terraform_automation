d7084458876fd7cf383f7f8482ae6e0e1a9f6fa6
Abhishek Garg <abhigarg28@gmail.com>
2022-05-08 14:46:03 -0700

    Creating my_local_setup repo.

--- live/github/repos.tf
+++ live/github/repos.tf
@@ -6,3 +6,11 @@ module "k8s_training_repo" {
   repo_name        = "k8s_training"
   repo_description = "Training repo for k8s"
 }
+module "my_local_setup_repo" {
+  providers = {
+    github = github
+  }
+  source           = "../../modules/github"
+  repo_name        = "my_local_setup"
+  repo_description = "keep my local setup files here"
+}
